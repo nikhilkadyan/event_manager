e39746b4dc3ff3d04bce335f504340e3917b679a
Nikhil Kadyan <nikhilkadyan@Nikhils-MacBook-Pro.local>
2020-12-26 00:02:29 +0530

    added navbar icons

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import './style.scss'
 
 import { NavLink } from 'react-router-dom'
+import { FaCalendar, FaUserAlt, FaTrophy, FaSignInAlt } from 'react-icons/fa';
+
 
 export default function index() {
     return (
         <div className="navbar_container">
             <span>EM</span>
             <div>
-                <NavLink activeClassName="active" to="/events">Events</NavLink>
-                <NavLink activeClassName="active" to="/authors">Authors</NavLink>
-                <NavLink activeClassName="active" to="/awards">Awards</NavLink>
-                <NavLink activeClassName="active" to="/auth">Auth</NavLink>
+                <NavLink activeClassName="active" to="/events"><FaCalendar /></NavLink>
+                <NavLink activeClassName="active" to="/authors"><FaUserAlt /></NavLink>
+                <NavLink activeClassName="active" to="/awards"><FaTrophy /></NavLink>
+                <NavLink activeClassName="active" to="/auth"><FaSignInAlt /></NavLink>
             </div>
         </div>
     )
